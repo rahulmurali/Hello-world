1be0bd44ab2467b7edc6134b1ae2978431afeace
Rahul Murali <rahulmurali07@gmail.com>
2018-10-19 14:10:44 +0530

    added sort.c

--- C/sort.c
+++ C/sort.c
@@ -1,50 +1,92 @@
-#include<stdio.h> 
-  
-void swap(int* a, int* b) 
-{ 
-    int temp = *a; 
-    *a = *b; 
-    *b = temp; 
-} 
-  
-void quickSort(int arr[], int low, int high) 
+#include<stdio.h>
+int i,j,temp=0;
+void bubblesort(int a[],int n)
 {
-    if (low >= high) return ; 
-    int pivot = arr[(low + high)/2];
-    int i = low;
-    int j = high;
-
-    while (i <= j) {
-        while (arr[i] < pivot) i++;
-        while (arr[j] > pivot) j--;
-
-        if (i <= j) {
-            swap(&arr[i], &arr[j]);
-            i++;
-            j--;
+	for(i=1;i<=n-1;i++)
+    	for(j=1;j<=n-i;j++)
+     	{
+       		if(a[j]>a[j+1])
+         	{
+           		temp=a[j];
+           		a[j]=a[j+1];
+           		a[j+1]=temp;
+          	}
+      	}
+}
+void selectionsort(int a[],int n)
+{
+	int min,minloc;
+   	for(i=1;i<=n-1;i++)
+   	{
+    	min=a[i];
+      	minloc=i;
+      	for(j=i+1;j<=n;j++)
+      	{
+         	if(min>a[j])
+         	{
+           		min=a[j];
+           		minloc=j;
+         	}
+       	}
+       	if(i!= minloc)
+       	{
+         	temp=a[minloc];
+         	a[minloc]=a[i];
+         	a[i]=temp;
+       	}
+   	}
+}
+void insertionsort(int a[], int n)
+{
+	int  k;
+   	for (i = 2; i <=n; i++)
+   	{
+    	for(j=1;j<=i;j++)
+        {
+        	if(a[j]>a[i])
+           	{
+             	temp=a[i];
+             	for(k=i;k>=j+1;k--)
+             	{
+               		a[k]=a[k-1];
+              	}
+              	a[j]=temp;
+              	break;
+            }
         }
     }
-    
-    if (low < j) quickSort(arr, low, j); 
-    if (i < high) quickSort(arr, i, high); 
-} 
-  
-void printArray(int arr[], int size) 
-{ 
-    int i; 
-    for (i=0; i < size; i++) 
-        printf("%d ", arr[i]); 
-    printf("\n"); 
-} 
-  
-int main() 
-{ 
-    //Sample: int arr[] = {10, 7, 8, 9, 1, 5}; 
-    int arr[] = { }; 
-    int n = sizeof(arr)/sizeof(arr[0]); 
-    quickSort(arr, 0, n-1); 
-    printf("Sorted array: \n"); 
-    printArray(arr, n); 
-    return 0; 
-} 
-
+}              
+void main()
+{  
+	int n,a[50], c;
+  	printf("\nenter the limit");
+  	scanf("%d",&n);
+   	printf("\nenter the elements");
+  	for(i=1;i<=n;i++)
+    	scanf("%d",&a[i]);
+  	do
+  	{
+    	printf("\n1.bubblesort\n2.selectionsort\n3.insertion\n4.Exit\n Enter Your  Choice :");
+        scanf("%d",&c);
+        switch(c)
+        {
+        	case 1: bubblesort(a,n);
+                printf("\nsorted ...... ");
+                for(i=1;i<=n;i++)    
+                	printf( "%d",a[i]);
+                break;
+            case 2: selectionsort(a,n);
+				printf("\nsorted ...... ");
+				for(i=1;i<=n;i++)    
+					printf( "%d",a[i]);
+				break;
+            case 3: insertionsort(a,n);
+				printf("\nsorted ...... ");
+				for(i=1;i<=n;i++)    
+					printf( "%d",a[i]);
+				break;
+            case 4:
+            	break;
+        }
+    }while(c!=4); 
+}
